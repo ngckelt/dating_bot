68c29e690c1b26d3b9dc6510169be7318c523c32
nick <kamarinskiv@mail.ru>
2021-08-31 21:05:24 +0700

    Добавил поиск пользователей и анкет в админке

--- admin/adminbot/admin.py
+++ admin/adminbot/admin.py
@@ -11,11 +11,18 @@ class SearchQuestionsAdmin(admin.ModelAdmin):
 
 
 class UsersAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['username']
+    search_fields = ['username', 'telegram_id']
+
+    class Meta:
+        model = models.Users
 
 
 class QuestionnairesAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['user__username', 'user__telegram_id']
+
+    class Meta:
+        model = models.Questionnaires
 
 
 admin.site.register(models.UserQuestions, UserQuestionsAdmin)
